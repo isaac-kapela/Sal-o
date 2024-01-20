21c0784f00372f8f543ea997634a2aa43806af67
Miguel Dias <miguel.dias@estudante.ufjf.br>
2024-01-20 17:46:36 -0300

    html modularizado

---
+++ scripts/menu.js
@@ -0,0 +1,7 @@
+menu = document.querySelector(".menu")
+        menu.addEventListener("click", () => {
+            
+            navBar = document.querySelector(".navBar")
+            navBar.classList.toggle("ativa")
+        
+        })
